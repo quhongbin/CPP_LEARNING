e7a5e22d85d86e79e5f2d57ad43e6a6d2dcd2bb4
arch-vmware <q2818777520@gamil.com>
2026-06-29 17:57:45 +0800

    update avl tree

--- Algorithm/AVL_TREE.cpp
+++ Algorithm/AVL_TREE.cpp
@@ -1,5 +1,7 @@
 #include <algorithm>
 #include <iostream>
+#include <list>
+#include <ostream>
 #include <queue>
 #include <vector>
 
@@ -21,58 +23,61 @@ TreeNode* rightRotation(TreeNode *cur_node) {
   return nullptr;
 }
 
+//calculate the balance factor from the root node
 int _balance_factor(TreeNode *cur_node) {
   if (cur_node == nullptr) {
     return -2;
   }
 
-  int l_child_h = cur_node->lnode ? cur_node->lnode->height : -1;
-  int r_child_h = cur_node->rnode ? cur_node->rnode->height : -1;
+  int l_child_h = cur_node->lnode ? cur_node->lnode->height + 1 : -1;
+  int r_child_h = cur_node->rnode ? cur_node->rnode->height + 1 : -1;
 
   return l_child_h - r_child_h;
 }
 
-TreeNode *balance_tree(TreeNode *cur_node) {
-  int bf = _balance_factor(cur_node);
-  TreeNode *root = cur_node;
-  if (bf == 2) {
-    TreeNode *non_bal_child = rightRotation(cur_node);
-  }
-  return nullptr;
-}
-
-int update_height(TreeNode* root){
+void update_height(TreeNode* root){
   if (root == nullptr) {
-    return 0;
+    root->height = -1;
   }
   int left_child_h = root->lnode ? root->lnode->height : -1;
   int right_child_h = root->rnode ? root->rnode->height : -1;
-  return std::max(left_child_h,right_child_h) + 1;  ;
+  root->height = std::max(left_child_h , right_child_h) + 1 ;
 }
 
+
+//insert node and keep balance
 void insert(TreeNode *root,int x) {
   TreeNode *node = new TreeNode(x);
+  std::list<TreeNode*> parents;
   while (true) {
     int mode = x < root->val ? 0 : 1;
     if (root == nullptr) {
       return;
     };
-    
+ 
     if (x < root->val && root->lnode == nullptr) {
       root->lnode = node;
-      root->height = update_height(root);
+      update_height(root);
       break;
     } else if(x > root->val && root->rnode == nullptr){
       root->rnode = node;
-      root->height = update_height(root);
+      update_height(root);
       break;      
     } else if(mode == 0){
-      root->height = update_height(root);
+      parents.push_front(root);
       root = root->lnode;
     } else {
-      root->height = update_height(root);
+      parents.push_front(root);
       root = root->rnode;
     }
+  }
+  for (TreeNode* &node : parents) {
+    update_height(node);
+  }
+  for (TreeNode* &node : parents) {
+    if (_balance_factor(node) > 1) {
+      node->lnode = rightRotation(node);
+    }    
   }
 }
 
